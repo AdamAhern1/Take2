b8fd9965a2800b4a5cdb25c92403687069b775d6
AdamAhern1 <125368481@umail.ucc.ie>
2026-09-16 21:06:01 +0100

    update

---
+++ Hello.py
@@ -0,0 +1,12 @@
+# My first python program
+print()
+print()
+name = input("What is your name? ")
+print ("Hello, " + name + "! Welcome to Engineering Python programming.")
+#blank line
+print()
+print()
+# calculate the area of a circle
+radius = float(input("Enter the radius of the circle: "))
+area = 3.14 * radius * radius
+print("The area of the circle with radius " + str(radius) + " is: " + str(area))
--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Deleted some lines in Git this time
 
 Adding a line to this file in VS to see if they update in Git
 Adding a line in Git ... did it make it to VS?
+
+this is all going wrong!
+
